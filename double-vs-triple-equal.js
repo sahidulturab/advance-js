99b078d77020541a1b49dba85b153da1dc56cf92
sahidulturab <sahidulturab.games@gmail.com>
2021-02-01 22:56:24 +0600

    sda

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,22 @@
+// const first=2
+// const second="2"
+// if(first===second){
+//     console.log("condition is True");
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+// double equal e string ar value  same hole true hoy / Means Doubke equal  Type check kore na,only value check
+// but triple equal e type same na hoile false dekhabe/means triple equal type check kore
+
+
+const first=1
+const second=true
+if(first==second){
+    console.log("condition is True");
+}
+else{
+    console.log("condition is false");
+}
+// double equal e true means 1 , 0 means False 
---
+++ null-vs-undefined.js
@@ -0,0 +1,38 @@
+// 1
+// let noValue;
+// console.log(noValue);
+
+
+// 2
+// // function add(num1,num2){
+// //     console.log(num1+num2);
+// //     // nothing is returned
+// // }
+// const result= add(13,82)
+// console.log(result);
+
+// 3
+// function add(num1,num2){
+
+//     console.log(num1,num2);
+// }
+// // 2nd parameter is not passed
+// const result= add(13)
+// console.log(result);
+
+
+// // 4
+// const Object= {name:"Turab", phone:10232}
+// console.log(object.id);
+// // id is not defined in object
+
+function doSomething(x, y){
+
+    console.log(y);
+    
+    }
+    
+    doSomething(32);
+
+
+// kichu value nai ba value nie erpor faka set korar jnne null use kora hoy
