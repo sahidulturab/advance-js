b39bcb0afe217227776215be363d33db55172017
sahidulturab <sahidulturab.games@gmail.com>
2021-02-01 22:37:56 +0600

    true false

---
+++ truthy-falsy.js
@@ -0,0 +1,18 @@
+// False
+// 0
+// ""
+// undefined = let name; means no value set
+// null
+// NaN
+// false
+
+// truth
+// "0". " ",[]
+
+const name=0;
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is False");
+}
